d1189c31141965f832d544bc3f49ad912ab97786
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2024-07-30 17:56:46 +0300

    Search and tests added

--- taxi/templatetags/query_transform.py
+++ taxi/templatetags/query_transform.py
@@ -7,9 +7,9 @@ register = template.Library()
 @register.simple_tag
 def query_transform(request, **kwargs):
     updated = request.GET.copy()
-    for k, v in kwargs.items():
-        if v is not None:
-            updated[k] = v
+    for key, vey in kwargs.items():
+        if vey is not None:
+            updated[key] = vey
         else:
-            updated.pop(k, 0)
+            updated.pop(key, 0)
     return updated.urlencode()
